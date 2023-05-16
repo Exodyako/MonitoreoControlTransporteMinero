1644d3c812a526dcd4f70414996235215cfc3f1d
Julian Sequeda <juliansequedaprofesional@gmail.com>
2023-05-15 21:18:35 -0500

    se modifica rutas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ConductorController;
+use App\Http\Controllers\MapController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');    
+    return redirect("login");
 });
+
+Route::get('login/', function () {
+    return "Pagina de login";
+});
+
+Route::get('users/{id}/perfil/{idPerfil?}', function ($id,$idPerfil = null) {
+    return "Entraste a al perfil del usuario ".$id."con id de perfil".($idPerfil== null ?" no definido":" ".$idPerfil);
+});
+
+Route::get('map', [MapController::class,"index"]);
+
+Route::get("conductor",[ConductorController::class,"index"]);
